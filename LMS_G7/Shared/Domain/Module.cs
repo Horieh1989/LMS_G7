4e75497915f5e7ef9f59ca3e21ff31d361db5cf5
Swetha Elanchezhiyan <eswetharajesh@gmail.com>
2024-01-02 13:40:32 +0100

    changed Guid to int for Id in Module Entity

--- LMS_G7/Shared/Domain/Module.cs
+++ LMS_G7/Shared/Domain/Module.cs
@@ -4,12 +4,12 @@ namespace LMS_G7.Shared.Domain;
 
 public class Module
 {
-	public Guid Id { get; set; }
+	public int ModuleId { get; set; }
 	public string Name { get; set; } = string.Empty;
 	public string Description { get; set; } = string.Empty;
 	public DateTime StartDate { get; set; }
 	public DateTime EndDate { get; set; }
 	public Guid? CourseId { get; set; }
-	//public Course? Course { get; set; }
+	public Course? Course { get; set; }
 	public ICollection<Activity>? Activities { get; set; }
 }
