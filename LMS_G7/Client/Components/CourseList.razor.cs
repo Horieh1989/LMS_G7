389cea98e543a10f3ef8614cb280379db837b344
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2024-01-03 06:36:19 +0100

    Remove component folder

--- LMS_G7/Client/Components/CourseList.razor.cs
+++ LMS_G7/Client/Components/CourseList.razor.cs
@@ -2,19 +2,19 @@
 using LMS_G7.Shared.Domain;
 using Microsoft.AspNetCore.Components;
 
-namespace LMS_G7.Client.Component
+namespace LMS_G7.Client.Components
 {
-    public  partial class CourseList
+    public partial class CourseList
     {
         [Parameter]
         public string ExtraCaption { get; set; } = string.Empty;// I should ask this
         [Inject]
         public ICourseDataService CourseDataService { get; set; }
-        public List<Course>CourseLst { get; set; }
+        public List<Course> CourseLst { get; set; }
         protected override void OnInitialized()
-        
+
         {
-            CourseLst=CourseDataService.GetCourses();
+            CourseLst = CourseDataService.GetCourses();
         }
     }
 }
