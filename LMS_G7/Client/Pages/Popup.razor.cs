bf4fac738f18edca9cfceff0b04d45b250bb0320
Jessie <jessiegrac12@gmail.com>
2024-01-03 16:29:51 +0100

    Included Popup window

---
+++ LMS_G7/Client/Pages/Popup.razor.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace LMS_G7.Client.Pages
+{
+    public partial class Popup
+    {
+        [Parameter] public bool IsVisible { get; set; }
+        [Parameter] public EventCallback<bool> IsVisibleChanged { get; set; }
+
+        private string EnteredText { get; set; }
+
+        private string selectedFileName;
+
+        private void ClosePopup()
+        {
+            IsVisible = false;
+            IsVisibleChanged.InvokeAsync(IsVisible);
+        }
+
+        private void SubmitForm()
+        {
+            // Handle form submission logic here
+            // For now, displaying an alert with the entered text
+            ClosePopup(); // Close the popup after submission
+
+        }
+
+        private void CancelForm()
+        {
+            ClosePopup();
+        }
+
+        private async Task HandleFileSelected(InputFileChangeEventArgs e)
+        {
+            var file = e.File;
+
+            if (file != null)
+            {
+                selectedFileName = file.Name;
+
+                // You can perform further actions with the file, such as uploading it to a server.
+                // For demonstration purposes, let's just print some information about the file.
+                Console.WriteLine($"File Name: {file.Name}");
+                Console.WriteLine($"File Size: {file.Size} bytes");
+            }
+        }
+    }
+}
--- LMS_G7/Shared/Activity.cs
+++ LMS_G7/Shared/Activity.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LMS_G7.Shared
 {
-    // ActivityModel.cs
+    
 
     public class ActivityModel
     {
