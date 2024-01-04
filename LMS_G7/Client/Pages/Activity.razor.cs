adb6033ab7b658db45b7bf87877238737c42fbe6
Jessie <jessiegrac12@gmail.com>
2024-01-04 10:32:21 +0100

    Updated Activity

--- LMS_G7/Client/Pages/Activity.razor.cs
+++ LMS_G7/Client/Pages/Activity.razor.cs
@@ -8,18 +8,53 @@ namespace LMS_G7.Client.Pages
         private ActivityModel newActivity = new ActivityModel();
 
         private bool elearnitems = false;
+        private bool lectureitems = false;
 
         private bool showActivity = true;
 
         private bool showPopup = false;
+        private bool practiceitems = false;
+        private bool assignitems = false;
         private void showelearn()
         {
             elearnitems = true;
+            showActivityItems = false;
+            lectureitems = false;
+            practiceitems = false;
+            assignitems = false;
+        }
+        private void showLecture()
+        {
+            lectureitems = true;
+            elearnitems = false;
+            showActivityItems = false;
+            practiceitems = false;
+            assignitems = false;
+        }
+        private void showSession()
+        {
+            practiceitems = true;
+            lectureitems = false;
+            elearnitems = false;
+            assignitems = false;
+            showActivityItems = false;
+        }
+        private void showAssign()
+        {
+            assignitems = true;
+            practiceitems = false;
+            lectureitems = false;
+            elearnitems = false;
+            showActivityItems = false;
         }
 
         private void ShowActivity()
         {
             showActivityItems = true;
+            elearnitems = false;
+            lectureitems = false;
+            practiceitems = false;
+            assignitems = false;
         }
 
         private void AddActivity()
@@ -36,8 +71,11 @@ namespace LMS_G7.Client.Pages
         private void OpenPopup()
         {
             showPopup = true;
+            showActivityItems = false;
+            elearnitems = false;
+            lectureitems = false;
         }
-
+                    
         private void HandlePopupVisibilityChange(bool isVisible)
         {
             showPopup = isVisible;
