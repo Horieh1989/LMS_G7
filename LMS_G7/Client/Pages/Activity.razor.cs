4950519a04caf354989fc5aabf6993301904590c
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2024-01-04 15:59:31 +0100

    Fix a bug

--- LMS_G7/Client/Pages/Activity.razor.cs
+++ LMS_G7/Client/Pages/Activity.razor.cs
@@ -5,18 +5,18 @@ namespace LMS_G7.Client.Pages
 {
     public partial class ActivityLst
     {
-        private bool showActivityItems = false;
-        private Activity newActivity = new Activity();
+        public bool showActivityItems = false;
+        public Activity newActivity { get; set; } = new Activity();
 
-        private bool elearnitems = false;
-        private bool lectureitems = false;
+        public bool elearnitems = false;
+        public bool lectureitems = false;
 
-        private bool showActivity = true;
+        public bool showActivity = true;
 
-        private bool showPopup = false;
-        private bool practiceitems = false;
-        private bool assignitems = false;
-        private void showelearn()
+        public bool showPopup = false;
+        public bool practiceitems = false;
+        public bool assignitems = false;
+        public void showelearn()
         {
             elearnitems = true;
             showActivityItems = false;
@@ -63,10 +63,11 @@ namespace LMS_G7.Client.Pages
             // await ActivityController.AddActivity(newActivity);
             // Add logic to save the newActivity to your local database or perform any other desired actions.
             // For simplicity, let's just print the details for now.
+            newActivity = new Activity();
             Console.WriteLine($"New Activity Added: {newActivity.Name}, {newActivity.ActivityType}, {newActivity.Description}, {newActivity.StartDate}, {newActivity.EndDate}");
 
             // Reset the form and hide the input fields
-            newActivity = new Activity();
+
             showActivityItems = false;
         }
         private void OpenPopup()
@@ -78,7 +79,7 @@ namespace LMS_G7.Client.Pages
             practiceitems = false;
             assignitems = false;
         }
-                    
+
         private void HandlePopupVisibilityChange(bool isVisible)
         {
             showPopup = isVisible;
