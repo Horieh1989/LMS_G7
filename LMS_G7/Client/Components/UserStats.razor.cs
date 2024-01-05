abd15966e510be6ed68e7d6bc25114f3135546ed
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2024-01-05 12:14:47 +0100

    Add UserStats

---
+++ LMS_G7/Client/Components/UserStats.razor.cs
@@ -0,0 +1,18 @@
+﻿using LMS_G7.Client.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace LMS_G7.Client.Components
+{
+    public partial class UserStats
+    {
+        [Inject]
+        public IUserDataService UserDataService { get; set; }
+        public int NumberOfUsers { get; set; }
+
+        protected override void OnInitialized()
+        {
+            NumberOfUsers = UserDataService.GetUsers().Count;
+            base.OnInitialized();
+        }
+    }
+}
