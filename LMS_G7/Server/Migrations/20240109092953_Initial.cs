7d1188dbc257c520d6ab4251efd5b6027896b395
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2024-01-09 10:34:51 +0100

    Final State

---
+++ LMS_G7/Server/Migrations/20240109092953_Initial.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS_G7.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Activities_ActivityType_ActivityTypeId",
+                table: "Activities");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ActivityType",
+                table: "ActivityType");
+
+            migrationBuilder.RenameTable(
+                name: "ActivityType",
+                newName: "ActivityTypes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Activities",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ActivityTypes",
+                table: "ActivityTypes",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Activities_ActivityTypes_ActivityTypeId",
+                table: "Activities",
+                column: "ActivityTypeId",
+                principalTable: "ActivityTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Activities_ActivityTypes_ActivityTypeId",
+                table: "Activities");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ActivityTypes",
+                table: "ActivityTypes");
+
+            migrationBuilder.RenameTable(
+                name: "ActivityTypes",
+                newName: "ActivityType");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Activities",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15,
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ActivityType",
+                table: "ActivityType",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Activities_ActivityType_ActivityTypeId",
+                table: "Activities",
+                column: "ActivityTypeId",
+                principalTable: "ActivityType",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
